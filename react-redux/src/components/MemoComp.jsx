b26113a3680aa7936be8ae1182e03419eea69cd5
KIMPPT <dudghks9303@gmail.com>
2023-05-12 12:43:39 +0900

    05121243

--- react-redux/src/components/MemoComp.jsx
+++ react-redux/src/components/MemoComp.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { addMemo, deleteMemo } from "../modules/memo";
 export default function MemoComp() {
   let memo = useSelector((state) => state.memo);
+  let dispatch = useDispatch();
+  let onAddMemo = () => {
+    dispatch(addMemo({ text: input, date: "2023-05-12" }));
+    setInput(""); //버튼 클릭시 dispatch후 input 값을 초기화
+  };
+  let [input, setInput] = useState("");
   return (
     <div>
       <h2>MemoComp</h2>
+      {/*form으로 만들어서 submit으로 실행 - form의 경우 Enter키를 눌려도 된다*/}
+      <form
+        onSubmit={(e) => {
+          //밑에 식이 없어서 버튼을 누르면 계속해서 새로고침이 되었음
+          e.preventDefault();
+          //이벤트에 바로 작성할 때는 함수자체를 넣어야 하기 때문에 onAddMemo로 넣어주지만, 지금 onSubmit에서 화살표 함수 안에 작성되고 있기에 onAddMemo()로 넣어야 한다
+          onAddMemo();
+        }}
+      >
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <button type="submit">form으로 메모 추가</button>
+      </form>
+      <input
+        type="text"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <button onClick={onAddMemo}>메모추가</button>
       {memo.map((m) => (
         <div key={m.id}>
           <p>{m.id}</p>
           <h4>{m.text}</h4>
           <p>{m.date}</p>
+          <button
+          //클릭했을 때 m.id를 action생성함수로 전달하여 reducer에서 filter를 통해 새 배열 만들기
+          onClick={()=>{dispatch(deleteMemo(m.id))}}>X</button>
         </div>
       ))}
       {/*
--- react-redux/src/modules/memo.js
+++ react-redux/src/modules/memo.js
@@ -18,12 +18,15 @@ let initalState = [
     date: "2023-05-13",
   },
 ];
+//화면에 출력 될 때 수정되어서 출력되지 않는 내용
+//내부적으로 쓰는 변수
+let id=3;
 
 //action 생성 함수
 //addMemo는 메모의 값을 추가하기 위해 함수를 통해서 memo의 값을 가져옴
 // : {text,date}를 가져와야함 > dispatch사용 때 확인
 export let addMemo = (memo) => ({ type: "ADD_MEMO", payload: memo });
-
+export let deleteMemo=(id)=>({type:"DELETE_MEMO",id:id});
 //reducer
 function memo(state = initalState, action) {
   switch (action.type) {
@@ -31,12 +34,17 @@ function memo(state = initalState, action) {
       //새로운 메모 작성
       let newMemo = {
         ...action.payload,
-        id: 3,
-      };
+        id:id,
+      }
+      id++;//마지막에 id 값을 1 증가시켜서 겹치지 않도록 함
       //새로운 메모 리스트 만들어서
       let newMemoList = state.concat(newMemo);
       //새로운 메모 리스트 return
       return newMemoList;
+    }
+    case "DELETE_MEMO":{
+        let deleteMemo=state.filter((m)=>m.id!==action.id)
+        return deleteMemo;
     }
     default: {
       return state;
