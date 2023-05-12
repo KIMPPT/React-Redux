d81c79e1f40eee601dd4e45923fb68b248869684
KIMPPT <dudghks9303@gmail.com>
2023-05-12 13:08:14 +0900

    05121308

--- react-redux/src/modules/memo.js
+++ react-redux/src/modules/memo.js
@@ -20,13 +20,13 @@ let initalState = [
 ];
 //화면에 출력 될 때 수정되어서 출력되지 않는 내용
 //내부적으로 쓰는 변수
-let id=3;
+let id = 3;
 
 //action 생성 함수
 //addMemo는 메모의 값을 추가하기 위해 함수를 통해서 memo의 값을 가져옴
 // : {text,date}를 가져와야함 > dispatch사용 때 확인
 export let addMemo = (memo) => ({ type: "ADD_MEMO", payload: memo });
-export let deleteMemo=(id)=>({type:"DELETE_MEMO",id:id});
+export let deleteMemo = (id) => ({ type: "DELETE_MEMO", id: id });
 //reducer
 function memo(state = initalState, action) {
   switch (action.type) {
@@ -34,17 +34,17 @@ function memo(state = initalState, action) {
       //새로운 메모 작성
       let newMemo = {
         ...action.payload,
-        id:id,
-      }
-      id++;//마지막에 id 값을 1 증가시켜서 겹치지 않도록 함
+        id: id,
+      };
+      id++; //마지막에 id 값을 1 증가시켜서 겹치지 않도록 함
       //새로운 메모 리스트 만들어서
       let newMemoList = state.concat(newMemo);
       //새로운 메모 리스트 return
       return newMemoList;
     }
-    case "DELETE_MEMO":{
-        let deleteMemo=state.filter((m)=>m.id!==action.id)
-        return deleteMemo;
+    case "DELETE_MEMO": {
+      let deleteMemo = state.filter((m) => m.id !== action.id);
+      return deleteMemo;
     }
     default: {
       return state;
