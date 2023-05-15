41710f7980d269de8ac8d9477857a681c5bfdd3c
KIMPPT <dudghks9303@gmail.com>
2023-05-15 16:56:12 +0900

    05151656

--- react-toolkit/src/components/MemoComp.jsx
+++ react-toolkit/src/components/MemoComp.jsx
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addMemoRedux, addMemoToolkit } from "../slices/memoSlice";
+import {
+  addMemoRedux,
+  addMemoToolkit,
+  deleteMemoRedux,
+  deleteSlice,
+} from "../slices/memoSlice";
 export default function MemoComp() {
   let memo = useSelector((state) => state.memo);
   let dispatch = useDispatch();
   let [memotext, setMemotext] = useState("");
   let [memotext2, setMemotext2] = useState("");
   console.log(memotext);
+  console.log(memo);
   return (
     <div>
-      {memo.map((m) => (
+      {memo.map((m, index) => (
+        //memo배열의 index 값을 전달하여 splice를 이용하여 삭제
+        //다양한 삭제 방법 중 하나
         <div key={m.id}>
           <h3>{m.text}</h3>
           <p>{m.date}</p>
+          <button onClick={() => dispatch(deleteSlice(index))}>
+            toolkit으로 만든 메모 제거
+          </button>
+          <button onClick={() => dispatch(deleteMemoRedux(m.id))}>
+            redux로 만든 메모 제거
+          </button>
         </div>
       ))}
       <button
--- react-toolkit/src/slices/memoSlice.js
+++ react-toolkit/src/slices/memoSlice.js
@@ -20,18 +20,33 @@ export let memoSlice = createSlice({
     },
     //값을 직접 수정해서 쓰는 방법
     addMemoToolkit: (state, action) => {
-        //배열의 메서드 중 push()
-        //push()는 원본 배열에 값을 추가해서 사용한다
-        let newMemo={
-            ...action.payload,
-            id:id,
-        }
-        id++;
-        state.push(newMemo);
+      //배열의 메서드 중 push()
+      //push()는 원본 배열에 값을 추가해서 사용한다
+      let newMemo = {
+        ...action.payload,
+        id: id,
+      };
+      id++;
+      state.push(newMemo);
+    },
+    deleteSlice: (state, action) => {
+      //state값을 바로 수정하기 위해 splice 사용
+      state.splice(action.payload, 1);
+      //이 경우 문제가 배열의 인덱스 번호가 수시로 변경된다는 점이다.
+    },
+    //반드시 toolkit에서 자공되는 형태로 해야 할 필요는 없다
+    //배열에 관한 값을 사용할 때는 익숙한 메서드 사용 권장
+    deleteMemoRedux: (state, action) => {
+      //1. id값을 이용하여 제외한 새로운 배열 생성
+      let newMemoList = state.filter((m) => m.id !== action.payload);
+      //2. return 새로운 배열을 할당
+      //3. return을 통해서 할당되는 형태는 state의 형태
+      return newMemoList;
     },
   },
 });
 //화면에 업데이트 되는 변수가 아닌, 코드안에서 사용할 변수
 let id = 2;
-export let { addMemoRedux,addMemoToolkit } = memoSlice.actions;
+export let { addMemoRedux, addMemoToolkit, deleteSlice, deleteMemoRedux } =
+  memoSlice.actions;
 export default memoSlice.reducer;
